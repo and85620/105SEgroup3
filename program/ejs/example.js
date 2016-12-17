900372a83439d2985f4a96099f1e5dad0dcfc937
Lynn <g9810177@gmail.com>
2016-12-18 04:01:46 +0800

    remove useless files

--- program/ejs/example.js
+++
@@ -1,4 +0,0 @@
-
-		var rddata = {tmps:['rank','like'],sortclasss:['googrk','likes'],fbgoogles:fbgoogle};
-		resultable = new EJS({url:"ejs/fbsrhresult.ejs"}).render(rddata);
-		$('#FBsrhResultup').html(resultable);
