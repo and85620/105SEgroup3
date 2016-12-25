ac62c38aa366f24d3e17419fd74e006218f023fd
and85620 <and85620@gmail.com>
2016-12-25 17:49:49 +0800

    sql

--- table new/store procedure.sql
+++ table new/store procedure.sql
@@ -1,14 +1,25 @@
 
+
 DELIMITER //
 create procedure  BuySellThing(Sid int,numberr int,stype int)
 begin 
 
-set @Mprice= (select price from material where mid = Sid);
-set @MMMoney= (select money from player limit 1);
-update player set money = (@MMMoney-@Mprice*numberr*stype);
+IF stype > 0 then
+    set @Mprice= (select price from material where mid = Sid);
+    set @MMMoney= (select money from player limit 1);
+    update player set money = (@MMMoney-@Mprice*numberr*stype);
 
-set @MMnum = (select num from material where mid = Sid);
-update material set num = (@MMnum + numberr*stype) where mid = Sid;
+    set @MMnum = (select num from material where mid = Sid);
+    update material set num = (@MMnum + numberr*stype) where mid = Sid;
 
+ELSE
+    set @Mprice= (select price from product where pid = Sid);
+    set @MMMoney= (select money from player limit 1);
+    update player set money = (@MMMoney-@Mprice*numberr*stype);
+
+    set @MMnum = (select num from product where pid = Sid) ;
+    update product set num = (@MMnum + numberr*stype) where pid = Sid;
+
+end IF;
 end//
-DELIMITER ;
+DELIMITER //
